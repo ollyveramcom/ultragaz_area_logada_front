4befe98e6e1508c57e409bfda799121c00fe01bb
ollyveramcom <ollyver.ottoboni@amcom.com.br>
2024-08-23 22:13:42 -0300

    feat: altera next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone", // Configura a saída para o modo standalone
+  swcMinify: false, // Habilita a minificação com SWC
+  poweredByHeader: false, // Desativa o cabeçalho X-Powered-By
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: true, // Redirecionamento permanente
+      },
+    ];
+  },
+};
 
 export default nextConfig;
